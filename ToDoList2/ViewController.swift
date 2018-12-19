38610d30c49cd149d4a6814cfdeeb377742ff3ac
Jun Dang <jun.dang8@gmail.com>
2018-12-18 19:51:02 -0500

    Initial Commit

---
+++ ToDoList2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList2
+//
+//  Created by Jun Dang on 2018-12-18.
+//  Copyright © 2018 Jun Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
